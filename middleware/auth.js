293b4a4615e9c8af737984e71ce2b1ec45336f1f
abidali671 <abid.saeed.ali92@gmail.com>
2023-09-20 20:01:33 +0500

    update middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,14 @@
+import jwt from "jsonwebtoken";
+import { Config } from "../utils/index.js";
+
 export default async function Auth(req, res, next) {
   try {
-    const token = req.headers.autherization;
-    res.json(token);
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded_token = jwt.verify(token, Config.jwtSecret);
+    req.user = decoded_token;
+
+    next();
   } catch (error) {
-    res.status(401).json({ error: "Autherization Failed" });
+    res.status(401).json({ error: "Authentication Failed" });
   }
 }
