b98219cbe29e4e9a14e5cd8590763e3bfe487676
abidali671 <abid.saeed.ali92@gmail.com>
2023-10-05 18:31:46 +0500

    category modal

---
+++ controller/index.js
@@ -0,0 +1,1 @@
+export { default as categoryController } from "./categoryController.js";
---
+++ model/Category.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Nam is required"],
+    unique: true,
+  },
+  slug: {
+    type: String,
+    required: [true, "Slug is required"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+  },
+});
+
+export default mongoose.model.Category ||
+  mongoose.model("Category", CategorySchema);
