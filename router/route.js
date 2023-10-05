75b5c1718a5abf2ee0613f069108821ac874938f
abidali671 <abid.saeed.ali92@gmail.com>
2023-10-05 18:31:58 +0500

    create: category routes

--- router/route.js
+++ router/route.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { RegisterMail } from "../controller/Mailer.js";
+import { categoryController } from "../controller/index.js";
+
 import Auth from "../middleware/auth.js";
 import VerifyUser from "../middleware/VerifyUser.js";
 import localVariables from "../middleware/localVariables.js";
-import { RegisterMail } from "../controller/Mailer.js";
+
 import * as controller from "../controller/appController.js";
 
 const router = Router();
@@ -16,24 +19,22 @@ router.route("/").get(function register(_, res) {
   });
 });
 
-// Post Methods
+// Authentication Routes
 router.route("/auth/register").post(controller.register, RegisterMail);
 router.route("/auth/register-mail").post(RegisterMail);
-// router.route("/auth/authenticate").post(controller);
 router.route("/auth/login").post(controller.login);
-
-// Get Methods
 router.route("/auth/verify").get(controller.verifyUser);
 router.route("/auth/user/:username").get(controller.getUser);
 router
   .route("/auth/generate-otp")
   .get(VerifyUser, localVariables, controller.generateOTP);
-
 router.route("/auth/verify-otp").get(VerifyUser, controller.verifyOTP);
 router.route("/auth/createResetSession").get(controller.createResetSession);
-
-// Put Methods
 router.route("/auth/user/:id").put(Auth, controller.updateUser);
 router.route("/auth/resetPassword").put(VerifyUser, controller.updateUser);
+
+// Category Routes
+router.route("/category").get(categoryController.getCategories);
+router.route("/category").post(categoryController.createCategory);
 
 export default router;
