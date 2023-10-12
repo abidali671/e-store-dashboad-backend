ccde82c5ef5bd618a6ad161c5012e932e72fa571
abidali671 <abid.saeed.ali92@gmail.com>
2023-10-12 20:47:15 +0500

    fix: dirname

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import cors from "cors";
 import morgan from "morgan";
 import path from "path";
 import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
 
 import connect from "./database/conn.js";
 import router from "./router/route.js";
 import Config from "./utils/Config.js";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
@@ -16,7 +19,7 @@ app.use(morgan("tiny"));
 app.disable("x-powered-by");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/images", express.static(path.join("images")));
+app.use("/images", express.static(__dirname + "/images"));
 app.use("/api", router);
 
 connect()
